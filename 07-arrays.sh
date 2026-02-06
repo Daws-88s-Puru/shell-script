4815b4f403bce2b2498b60a8c5d17db28a0ffd0b
purusreddy <c.nagapurushothamreddy@gmail.com>
2026-02-06 14:18:00 +0530

    shell-script

---
+++ 07-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("pushpa","rrr","devara)
+#index starts from 0 and size is 3
+
+echo "First movie :${MOVIES[1]}"
+echo "Second movie :${MOVIES[2]}"
+echo "Third movie :${MOVIES[2]}"
+
+echo "All movies ":${MOVIES[@]}
