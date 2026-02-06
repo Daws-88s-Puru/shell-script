0615ab3811cd92e0d9be1ab7122e7b2f0c5a60df
purusreddy <c.nagapurushothamreddy@gmail.com>
2026-02-06 12:32:27 +0530

    convertions added

---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "please enter your username" #text entered here will be added as value to variable
+
+read -s USERNAME #here username is variable name
+
+echno "username entered: $USERNAME"
