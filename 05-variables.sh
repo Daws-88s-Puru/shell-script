52cffd2e6b280e6d98a578b5d8d22fb921bd77a1
purusreddy <c.nagapurushothamreddy@gmail.com>
2026-02-06 12:34:47 +0530

    convertions added

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "please enter your username" #text entered here will be added as value to variable
-
 read -s USERNAME #here username is variable name
-
 echo "username entered: $USERNAME"
+echo "please enter your passwprd"
+read -s Password
